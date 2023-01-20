8dc0713bdae7926584dfd3543020c4ed27687b45
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-01-20 19:00:49 +0000

    added pseudocode

---
+++ challenge/starter/dailyplanner.js
@@ -0,0 +1,30 @@
+// Display the current day at the top of the calender when a user opens the planner.
+    
+    // moment js today
+
+// Present timeblocks for standard business hours when the user scrolls down.
+
+    // create li for time blocks with jQuery 
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+// Color using logic  check out Activities 7.1.3 and 7.1.4 - attach functionality to events. Also, use the .css() method.
+
+// Allow a user to enter an event when they click a timeblock
+
+// Check out how to do that in Activities 7.1.5 and 7.1.6 - Working with forms - reading data from inputs; working with checkboxes
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+
+// Check out LocalStorage
+
+// Persist events between refreshes of a page
+
+//Prevent Default
+
+// The following animation demonstrates the application functionality:
+
+
+
+// A user clicks on slots on the color-coded calendar and edits the events.
+
